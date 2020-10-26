46cc70bf2c46c428049fb39953e9dd47f94578eb
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2020-10-25 21:47:46 -0500

    import code from old project

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Items
+{
+    abstract class Item
+    {
+        public Entity owner;
+        public string spriteDir;
+
+        public abstract int Mass { get; }
+        public abstract int ThrownDamage { get; }
+
+        public abstract void Eat(out int saturation);
+        public abstract void Apply(Item other);
+        public abstract void Throw(float momentum, ref int damage, Collision2D collision);
+        public abstract void Wield(out bool success);
+        public abstract void UnWield(out bool success);
+        public abstract void Strike(ref bool hit, ref int damage);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Tooltip("Kinematic Rigidbody")]
+    public Rigidbody2D rb;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(xAxis, yAxis) * speed;
+    }
+}
