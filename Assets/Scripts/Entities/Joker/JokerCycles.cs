a8d8a66d04d337e46ce0cbe277d57725460ff253
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2020-10-29 19:19:24 -0500

    boss complete? maybe

--- Assets/Scripts/Entities/Joker/JokerCycles.cs
+++ Assets/Scripts/Entities/Joker/JokerCycles.cs
@@ -30,5 +30,105 @@ public partial class Joker
     public Path wiggle8 => Negy(wiggle2);
 
     #endregion
+    #region Cycles
+
+
+    Cycle TPAndStartMain(Transform location) => (ref Cycle self) =>
+    {
+        transform.position = location.position;
+
+        boomerangTimer.Use();
+        altTimer.Use();
+        mainTimer.Use();
+
+        self = MainCycler;
+        action = Phase1;
+        altCycle = FireAtTarget;
+
+        return -1;
+    };
+    float MainCycler(ref Cycle self)
+    {
+        action = Phase1;
+        self = Cycle2;
+        return -1;
+    }
+    float Cycle2(ref Cycle self)
+    {
+        action = BoomerangCycle;
+        self = MainCycler;
+        return -1;
+    }
+
+    float Phase1(ref Cycle action)
+    {
+        //do somthing
+        FireBoomerangs(path1, path2);
+
+        action = Phase2;
+        return -1;
+    }
+    float Phase2(ref Cycle action)
+    {
+        //do somthing
+        FireBoomerangs(Path3, Path4);
+
+        action = Phase3;
+        return -1;
+    }
+    float Phase3(ref Cycle action)
+    {
+        //do somthing
+        FireBoomerangs(Path5, Path6);
+
+        action = Phase4;
+        return -1;
+    }
+    float Phase4(ref Cycle action)
+    {
+        //do somthing
+        FireBoomerangs(Path7, Path8);
+
+        action = Phase1;
+        return 1.5f;
+    }
+
+    float BoomerangCycle(ref Cycle action)
+    {
+        FireBoomerangs(wiggle, wiggle2);
+
+        action = Boomer2;
+        return -1;
+    }
+    float Boomer2(ref Cycle action)
+    {
+        FireBoomerangs(wiggle3, wiggle4);
+
+        action = Boomer3;
+        return -1;
+    }
+    float Boomer3(ref Cycle action)
+    {
+        FireBoomerangs(wiggle5, wiggle6);
+
+        action = Boomer4;
+        return -1;
+    }
+    float Boomer4(ref Cycle action)
+    {
+        FireBoomerangs(wiggle7, wiggle8);
+
+        action = BoomerangCycle;
+        return -1;
+    }
+
+    float FireAtTarget(ref Cycle _)
+    {
+        this.FireAt(testTarget.transform.position - this.transform.position);
+
+        return -1;
+    }
+
+    #endregion
 }
 
---
+++ Assets/Scripts/Util/NoHealthBar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class NoHealthBar : HealthBar
+{
+    //lol
+    protected override void SetHealthBar(int hp, int HP)
+    {
+        return;
+    }
+}
